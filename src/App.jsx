181e7d0ddc35a3249f8b64279c644d1a222e7128
CintiaMurashima <cintiamurashima@gmail.com>
2023-04-03 15:55:43 +0200

    edit & delete

--- src/App.jsx
+++ src/App.jsx
@@ -9,75 +9,141 @@ import NuevoUsuario from './NuevoUsuario';
 
 function App() {
 
-//   const usuarios = [
-//     { "id": 1, "nombre": "Quim", "email": "quim@unmail.com" },
-//     { "id": 2, "nombre": "Terenci", "email": "terenci@unmail.com" },
-//     { "id": 3, "nombre": "Josep", "email": "josep@unmail.com" },
-//     { "id": 4, "nombre": "Baltasar", "email": "baltasar@unmail.com" }
-// ]
-
   const URL = 'http://localhost:3000/api/usuarios';
 
+  const [index, setIndex] = useState(-1);
+  const [id, setId] = useState("");
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [lista, setLista] = useState([]);
 
 
-  function getUsers(){
-    console.log("cargando...")
-
+  function getUsers() {
     fetch(URL)
       .then(resp => resp.json())
       .then(resp => setLista(resp.data))
-      .catch(error=>console.log(error))
-
-    console.log("despues del fetch...",lista)
+      .catch(error => console.log(error))
   }
 
-  useEffect(()=>{
+  useEffect(() => {
     getUsers();
   }, [])
 
 
+  function borraItem(x) {
+    const nuevaLista = [...lista]
+    setId(lista[x].id)
+    console.log("delete",id)
+    deleteUsu(id);
+    nuevaLista.splice(x, 1)
+    setLista(nuevaLista)
+  }
+
+  function editaItem(x) {
+    setIndex(x)
+    console.log("index edita",index)
+    setId(lista[x].id)
+    setName(lista[x].name)
+    setEmail(lista[x].email)
+  }
+
   function guardaItem() {
     const nuevaLista = [...lista]
-    nuevaLista.push({ name, email })
-    setLista(nuevaLista)
-    
-    //reseteando campos
-    setName("")
-    setEmail("")
+    if (index > -1) {
+      console.log("entra a guardarITEM", index, name, email)
+      //estamos editando
+      nuevaLista[index].name = name
+      nuevaLista[index].email = email
+      console.log("id", id, name, email)
+      editar(id);
+    } else {
+      //registro nuevo
+      nuevaLista.push({ name, email })
+      guardarUsu(name, email);
     }
 
+    //actualizando lista
+    setLista(nuevaLista)
+    //reseteando campos
+    setId()
+    setName("")
+    setEmail("")
+    setIndex(-1)
+  }
+
+  function deleteUsu(id){
+    console.log("index delete",id, (URL + "/" + id))
+    fetch((URL + "/" + id), {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({id,name, email})
+    })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+  
+  function guardarUsu(name, email) {
+    fetch(URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ name: name, email: email })
+    })
+      .then(response => response.text())
+      .then(data => console.log(data))
+      .catch(error => console.error(error));
+  }
+  function editar(id) {
+    fetch((URL+"/"+ id ), {
+      method: 'PUT',
+      body: JSON.stringify({name, email}),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+      .then(response => response.json())
+      .then(date => {
+        console.log('Success:', date);
+      })
+      .catch((error) => {
+        console.error('Error:', error);
+      });
+  }
+
+
   return (
-      <BrowserRouter>
-    
-        <Container>
-          <Navbar bg="light" expand="lg">
-            <Container>
-              <Link to="/" classemail="navbar-brand">APLICACION</Link>
-              <Navbar.Toggle aria-controls="basic-navbar-nav" />
-              <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
-                  <Link to="/" className='nav-link'>Home</Link>
-                  <Link to="/usuarios/lista" className='nav-link'>Lista</Link>
-                  <Link to="/usuarios/nuevo" className='nav-link'>Nuevo</Link>
-                </Nav>
-              </Navbar.Collapse>
-            </Container>
-          </Navbar>
-          <hr />
-          <br />
-          <br />
+    <BrowserRouter>
 
-          <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/usuarios/lista"  element={<ListaUsuarios lista={lista} setLista={setLista} />}/>
-            <Route path="/usuarios/nuevo" element={<NuevoUsuario name={name} setName={setName} email={email} setEmail={setEmail} guardaItem={guardaItem}/>} />
-          </Routes>
+      <Container>
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Link to="/" classemail="navbar-brand">APLICACION</Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Link to="/" className='nav-link'>Home</Link>
+                <Link to="/usuarios/lista" className='nav-link'>Lista</Link>
+                <Link to="/usuarios/nuevo" className='nav-link'>Nuevo</Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+        <hr />
+        <br />
+        <br />
 
-        </Container>
-      </BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/usuarios/lista" element={<ListaUsuarios lista={lista} setLista={setLista} borraItem={borraItem} editaItem={editaItem} />} />
+          <Route path="/usuarios/nuevo" element={<NuevoUsuario name={name} setName={setName} email={email} setEmail={setEmail} guardaItem={guardaItem} />} />
+        </Routes>
+
+      </Container>
+    </BrowserRouter>
   )
 }
 
--- src/ListaUsuarios.jsx
+++ src/ListaUsuarios.jsx
@@ -1,16 +1,35 @@
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrashCan, faEdit } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom";
+
 
 function ListaUsuarios(props) {
 
-  
+    const navigate = useNavigate();
+
+    function edita(index){
+        props.editaItem(index);
+        navigate('/usuarios/nuevo');
+    }
 
     const listaFilas = props.lista.map((el, index) => (
         <tr key={index}>
-          <td>{index+1}</td>
-          <td>{el.name}</td>
-          <td>{el.email}</td>
+            <td>{index + 1}</td>
+            <td>{el.name}</td>
+            <td>{el.email}</td>
+            <td>
+                <Button size="sm" variant="primary" onClick={() => edita(index)} >
+                    <FontAwesomeIcon icon={faEdit} />
+                </Button>
+                <Button size="sm" variant="danger" onClick={() => props.borraItem(index)} >
+                    <FontAwesomeIcon icon={faTrashCan} />
+                </Button>
+            </td>
+
         </tr>))
-    
+
     return (
         <>
             <Table striped bordered hover>
@@ -19,10 +38,11 @@ function ListaUsuarios(props) {
                         <th>Id</th>
                         <th>Nombre</th>
                         <th>Email</th>
+                        <th>Acciones</th>
                     </tr>
                 </thead>
                 <tbody>
-                {listaFilas}
+                    {listaFilas}
                 </tbody>
             </Table>
         </>
